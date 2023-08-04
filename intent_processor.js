8a2ffe5f1341e0c10798c1c542fc2e5bcbff8cfd
Harsha <harrysvallabhaneni.1@gmail.com>
2023-08-04 17:55:25 +0530

    all the intents removed from directory added

--- intent_processor.js
+++ intent_processor.js
@@ -1,47 +1,74 @@
+const fs = require('fs');
+const path = require('path');
 const reader = require('xlsx');
-const filePath = './distance csv files/shift.csv';
 
-const file = reader.readFile(filePath);
-const sheet = file.Sheets['Sheet1'];
-const jsonSheet = reader.utils.sheet_to_json(sheet);
+// Function to process a single file
+function processFile(filePath) {
+  const file = reader.readFile(filePath);
+  const sheet = file.Sheets['Sheet1'];
+  const jsonSheet = reader.utils.sheet_to_json(sheet);
 
-const columnNames = Object.keys(jsonSheet[0]).filter(column => column !== '__EMPTY');
+  const columnNames = Object.keys(jsonSheet[0]).filter(column => column !== '__EMPTY');
 
-const set = new Set();
-const checkedPairs = new Set(); // Keep track of checked row-column pairs
+  const removedIntents = new Set();
+  const checkedPairs = new Set(); // Keep track of checked row-column pairs
 
-jsonSheet.forEach((row, rowIndex) => {
-  columnNames.forEach((column, columnIndex) => {
-    if (
-      rowIndex === columnIndex ||
-      rowIndex < 0 || columnIndex < 0 ||
-      checkedPairs.has(`${rowIndex}-${columnIndex}`)
-    ) {
-      return; // Skip this iteration
-    }
+  jsonSheet.forEach((row, rowIndex) => {
+    columnNames.forEach((column, columnIndex) => {
+      if (
+        rowIndex === columnIndex ||
+        rowIndex < 0 || columnIndex < 0 ||
+        checkedPairs.has(`${rowIndex}-${columnIndex}`)
+      ) {
+        return; // Skip this iteration
+      }
 
-    checkedPairs.add(`${rowIndex}-${columnIndex}`); // Mark this pair as checked
+      checkedPairs.add(`${rowIndex}-${columnIndex}`); // Mark this pair as checked
 
-    const distance = parseFloat(row[column]);
-    const originalUtterance = row['__EMPTY'];
-    
-    if (distance < 0.1) {
-      console.log(`Identified low blow: ${originalUtterance} VS ${column} with a distance ${distance}`);
+      const distance = parseFloat(row[column]);
+      const originalUtterance = row['__EMPTY'];
 
-      if (set.has(originalUtterance) && set.has(column)) {
-        console.log("TWO CONFLICTING UTTERANCES ALREADY IN SET, NOT IDEAL");
-      } else if (set.has(originalUtterance) || set.has(column)) {
-        // One is present, ignore
-      } else {
-        // None present, add any one to set
-        if (Math.random() > 0.5) {
-          set.add(originalUtterance);
+      if (distance < 0.1) {
+        if (removedIntents.has(originalUtterance) && removedIntents.has(column)) {
+          // Do something with conflicting intents
+        } else if (removedIntents.has(originalUtterance) || removedIntents.has(column)) {
+          // One is present, ignore
         } else {
-          set.add(column);
+          // None present, add any one to removedIntents
+          if (Math.random() > 0.5) {
+            removedIntents.add(originalUtterance);
+          } else {
+            removedIntents.add(column);
+          }
         }
       }
+    });
+  });
+
+  return Array.from(removedIntents);
+}
+
+// Function to process all files in a directory
+function processFilesInDirectory(directoryPath) {
+  const intentResults = [];
+
+  fs.readdirSync(directoryPath).forEach(file => {
+    if (file.endsWith('.csv')) {
+      const filePath = path.join(directoryPath, file);
+      const intentName = path.basename(file, '.csv');
+      const intentsRemoved = processFile(filePath);
+      intentResults.push({
+        intentName,
+        fileName: file,
+        intentsKept: [], // Implement logic for intents to be kept
+        intentsRemoved,
+      });
     }
   });
-});
 
-console.log(set);
+  return intentResults;
+}
+
+const directoryPath = './distance csv files'; // Update this with your directory path
+const results = processFilesInDirectory(directoryPath);
+console.log(results);
